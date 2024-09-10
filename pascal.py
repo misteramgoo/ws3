e1990cb4c0219e6a31f39457c254696f162ac3ce
Mister Magoo <d1liadvo@skola.goteborg.se>
2024-09-10 15:32:59 +0200

    python

--- pascal.py
+++ pascal.py
@@ -1,12 +1,10 @@
 from math import sqrt, log10, floor, pi
-from sys import argv
-
+from sys import argv, exit
 
 def pascal_triangle(n: int) -> None:
     """Prints a pascal triangle with n rows."""
     max_value_size: int = _pascal_size_max(n)
     _pascal_helper(1, n, [1], max_value_size)
-
 
 def _pascal_size_max(row: int) -> int:
     """Calculates an approximation of the number of digits in the largest
@@ -21,7 +19,6 @@ def _pascal_size_max(row: int) -> int:
     approx_bin_coff: float = 2**(2 * n) / sqrt(pi * n)
     return floor(log10(approx_bin_coff)) + 1
 
-
 def _pascal_helper(current: int, max: int, row: list[int], max_value_size: int) -> None:
     """Given a row of the pascal triangle, print it and calculate the next.
        Recurse and continue until current has reached max."""
@@ -30,21 +27,21 @@ def _pascal_helper(current: int, max: int, row: list[int], max_value_size: int) 
 
     print()
 
-    if current < max:
-        sums: list[int] = [a + b for a, b in zip(row, row[1:])]
-        new_row: list[int] = [1, *sums, 1]
-        _pascal_helper(current + 1, max, new_row, max_value_size)
+# Kontrollera om ett giltigt antal rader har angetts på kommandoraden
+if len(argv) != 2:
+    print("Användning: python3 pascal.py <antal_rader>")
+    exit(1)
 
+try:
+    n = int(argv[1])
+except ValueError:
+    print("Fel: <antal_rader> måste vara ett heltal.")
+    exit(1)
 
-def main():
-    if len(argv) == 2:
-        try:
-            pascal_triangle(int(argv[1]))
-        except ValueError:
-            print("Program argument isn't a number!")
-    else:
-        pascal_triangle(6)
+# Kontrollera om n är inom det giltiga intervallet
+if n < 1 or n > 1023:
+    print("Fel: Antal rader måste vara mellan 1 och 1023.")
+    exit(1)
 
-
-if __name__ == '__main__':
-    main()
+# Om n är giltigt, skriv ut Pascal's triangel
+pascal_triangle(n)
